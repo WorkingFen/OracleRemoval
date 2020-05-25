0fb8d4d89bc35f6e2567fe698e55da744096bc58
DeadFenris <theworkingfen@gmail.com>
2020-05-25 23:00:29 +0200

    Before first usage

--- UNINSTALL.bat
+++ UNINSTALL.bat
@@ -7,7 +7,7 @@ exit /b %ERRORLEVEL%
 
 :: Remove Oracle's environment variable
 :RemoveOracleEnvVariable
-set $line=%PATH%
+call :SystemPath $line
 set $line=%$line: =#%
 set $line=%$line:;= %
 set $line=%$line:)=^^)%
@@ -17,44 +17,36 @@ for %%a in (%$line%) do (
 )
 set $newpath=!$newpath:#= !
 set $newpath=!$newpath:^^=!
-:: echo set path=!$newpath:~1!
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "!$newpath:~1!" /f >nul
 exit /b 0
 
 :: Remove Oracle's registries
 :RemoveOracleRegs
 for /f "usebackq" %%i in (`reg query HKLM\SOFTWARE\ORACLE /K /F ora`) do (
-	echo %%i | find /i "End" >nul || echo %%i
+	echo %%i | find /i "End" >nul || reg delete %%i /f >nul
 )
-:: reg delete %%i /f >nul
-
 for /f "usebackq" %%i in (`reg query HKLM\SOFTWARE\ORACLE /K /C /F ODP`) do (
-	echo %%i | find /i "End" >nul || echo %%i
+	echo %%i | find /i "End" >nul || reg delete %%i /f >nul
 )
-:: reg delete %%i /f >nul
-
 for /f "usebackq" %%i in (`reg query HKLM\SYSTEM\CurrentControlSet\Services /K /C /F ORCL`) do (
-	echo %%i | find /i "End" >nul || echo %%i
+	echo %%i | find /i "End" >nul || reg delete %%i /f >nul
 )
-:: reg delete %%i /f >nul
-
 for /f "usebackq" %%i in (`reg query HKLM\SYSTEM\CurrentControlSet\Services /K /C /F OraDB`) do (
-	echo %%i | find /i "End" >nul || echo %%i
+	echo %%i | find /i "End" >nul || reg delete %%i /f >nul
 )
-:: reg delete %%i /f >nul
 exit /b 0
 
 :: Remove Oracle's main directory
 : RemoveOracleMainDir
-:: rmdir /Q /S %dir:~0,7%
+rmdir /Q /S %dir:~0,7%
 exit /b 0
 
 :: Remove Oracle's start menu directory
 : RemoveOracleMenuDir
 set menuDir=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\
 for /f "usebackq delims=" %%i in (`dir "%menuDir%" /A:D /B ^| find "OraDB" `) do (
-	echo %menu_dir%%%i
+	rmdir /Q /S %menu_dir%%%i
 )
-:: rmdir /Q /S %menu_dir%%%i
 exit /b 0
 
 :: Open computer management to delete manually all additional users
@@ -82,7 +74,7 @@ call :RemoveOracleRegs
 echo ###### Make sure all your open files are saved. ####
 echo ###### All unsaved changes will be lost. ###########
 pause
-:: shutdown /r /t 0 /d p:4:1
+shutdown /r /t 0 /d p:4:1
 exit /b 0
 
 :: Last section which will be executed
@@ -94,4 +86,13 @@ call :RemoveOracleMenuDir
 call :OpenUsers
 echo ###### All your Oracle data has been deleted. ######
 pause
+exit /b 0
+
+:: Get system PATH
+:SystemPath <SystemPath>
+for /f "skip=2 tokens=1,2*" %%n in ('reg query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path 2^>nul') do (
+    if /i "%%n" == "Path" (
+        set %~1=%%p
+    )
+)
 exit /b 0
